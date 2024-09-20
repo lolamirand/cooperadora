3185879423abc24538a91b5d26a69fd8828d139c
lolamirand <karendelcastillo73@gmail.com>
2024-09-20 10:44:55 -0300

    Diseño del formulario frmPagos

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.cooperadora.Form1
+            Me.MainForm = Global.cooperadora.frmPagos
         End Sub
     End Class
 End Namespace

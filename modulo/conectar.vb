40bfedc8a427829aa4576e0155a53212dbd163b4
lolamirand <karendelcastillo73@gmail.com>
2024-09-20 11:04:32 -0300

    incorporación del archivo psra conectar a la base de datos

---
+++ modulo/conectar.vb
@@ -0,0 +1,36 @@
+﻿
+Imports MySqlConnector
+
+Module conectar
+    Public Function GetConnection() As MySqlConnection
+        Dim connectionString As String = "server=localhost;port=3308;user=root;password=;database=cooperadora"
+        Return New MySqlConnection(connectionString)
+    End Function
+
+    Public Sub ExecuteNonQuery(query As String)
+        Using connection As MySqlConnection = GetConnection()
+            Try
+                connection.Open()
+                Dim command As New MySqlCommand(query, connection)
+                command.ExecuteNonQuery()
+            Catch ex As Exception
+                MessageBox.Show("Error de conexión: " & ex.Message)
+            End Try
+        End Using
+    End Sub
+
+    Public Function ExecuteQuery(query As String) As DataTable
+        Dim dataTable As New DataTable()
+        Using connection As MySqlConnection = GetConnection()
+            Try
+                connection.Open()
+                Dim adapter As New MySqlDataAdapter(query, connection)
+                adapter.Fill(dataTable)
+            Catch ex As Exception
+                MessageBox.Show("Error de conexión: " & ex.Message)
+            End Try
+        End Using
+        Return dataTable
+    End Function
+
+End Module
